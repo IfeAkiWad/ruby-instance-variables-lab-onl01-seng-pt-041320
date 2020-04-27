9ccd52c098dc4abba85a6863588a3f455eace893
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-27 22:34:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
+#we are going to define two instance methods below.
+
 class Dog
-  def name=(dog_name)
+  def name=(dog_name) #instance method 1
     this_dogs_name = dog_name
   end
 end
 
-def name
+def name #instance method 2
   this_dogs_name
 end
