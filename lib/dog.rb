e41dc4f56eab96e7e2edca614d07ed80dd9284a1
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-27 22:38:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
 #we are going to define two instance methods below.
 
 class Dog
-  def name=(dog_name) #instance method 1
-    this_dogs_name = dog_name
+  def name=(dog_name) #instance method 1. dog_name is an argument of a dog's name.
+    this_dogs_name = dog_name #we set the dog's name equal to a variable.
   end
 end
 
-def name #instance method 2
+def name #instance method 2 => responsible for reporting, or reading the name. 
   this_dogs_name
 end
+
+#The methods act as mechanisms to expose the data internally to the outside world. They are responsible for setting and getting 
+#an individual dog's name.
